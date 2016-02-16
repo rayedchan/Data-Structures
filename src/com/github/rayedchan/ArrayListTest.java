0fa32516dd900a0b4a0442dcfcbd30633ca6e94c
Ray Chan <rayedchan@gmail.com>
2016-02-15 20:55:26 -0500

    Completed ArrayList Implementation

---
+++ src/com/github/rayedchan/ArrayListTest.java
@@ -0,0 +1,163 @@
+package com.github.rayedchan;
+
+import static org.junit.Assert.*;
+import java.util.Arrays;
+import java.util.NoSuchElementException;
+
+import org.junit.Test;
+import com.github.rayedchan.datastructures.ArrayList;
+
+public class ArrayListTest 
+{
+	@Test
+	public void testAdd()
+	{
+		 ArrayList l = new ArrayList(4);
+	 	 l.add("a");
+	 	 l.add("b");
+	 	 l.add("c");
+	 	 l.add("d");
+	 	 //System.out.println(l);
+	 	 assertEquals(Arrays.toString(new Object[]{"a", "b", "c", "d"}), l.toString());
+	}
+	
+	@Test
+	public void testExpand()
+	{
+		 ArrayList l = new ArrayList(2);
+	 	 l.add(1);
+	 	 l.add(2);
+	 	 l.add(3);
+	 	 l.add(4);
+	 	 l.add(5);
+	 	 //System.out.println(l);
+	 	 assertEquals(Arrays.toString(new Object[]{1, 2, 3, 4, 5, null}), l.toString());
+	}
+	
+	@Test
+	public void testInsertAt()
+	{
+		 ArrayList l = new ArrayList(5);
+		 l.add("a");
+	 	 l.add("b");
+	 	 l.add("c");
+	 	 l.add("d");
+	 	 l.insertAt("x", 2);
+	 	 //System.out.println(l);
+	 	 assertEquals(Arrays.toString(new Object[]{"a", "b", "x", "c", "d"}), l.toString());
+	}
+	
+	@Test
+	public void testInsertAtExpand()
+	{
+		 ArrayList l = new ArrayList(4);
+		 l.add("a");
+	 	 l.add("b");
+	 	 l.add("c");
+	 	 l.add("d");
+	 	 l.insertAt("x", 0);
+	 	 //System.out.println(l);
+	 	 assertEquals(Arrays.toString(new Object[]{"x", "a", "b", "c", "d", null}), l.toString());
+	}
+	
+	@Test
+	public void testRemove()
+	{
+		 ArrayList l = new ArrayList(4);
+		 l.add("a");
+	 	 l.add("b");
+	 	 l.add("c");
+	 	 l.add("d");
+	 	 l.remove();
+	 	 //System.out.println(l);
+	 	 assertEquals(Arrays.toString(new Object[]{"a", "b", "c", null}), l.toString());
+	}
+	
+	@Test
+	public void testIndexOf()
+	{
+		 ArrayList l = new ArrayList(4);
+		 l.add("a");
+	 	 l.add("b");
+	 	 l.add("c");
+	 	 l.add("d");
+	 	 int result = l.indexOf("b");
+	 	 //System.out.println(result);
+	 	 assertEquals(1, result);
+	}
+	
+	@Test
+	public void testIndexOfNotFound()
+	{
+		 ArrayList l = new ArrayList(4);
+		 l.add("a");
+	 	 l.add("b");
+	 	 l.add("c");
+	 	 l.add("d");
+	 	 int result = l.indexOf("z");
+	 	 //System.out.println(result);
+	 	 assertEquals(-1, result);
+	}
+	
+	@Test
+	public void testRemoveAt()
+	{
+		 ArrayList l = new ArrayList(4);
+		 l.add("a");
+	 	 l.add("b");
+	 	 l.add("c");
+	 	 l.add("d");
+	 	 l.removeAt(1);
+	 	 //System.out.println(l);
+	 	 assertEquals(Arrays.toString(new Object[]{"a","c", "d", null}), l.toString());
+	}
+	
+	@Test(expected=IndexOutOfBoundsException.class)
+	public void testRemoveAtException()
+	{
+		 ArrayList l = new ArrayList(4);
+		 l.add("a");
+	 	 l.add("b");
+	 	 l.add("c");
+	 	 l.add("d");
+	 	 l.removeAt(20);
+	}
+	
+	@Test
+	public void testRemoveElement()
+	{
+		 ArrayList l = new ArrayList(4);
+	 	 l.add(1);
+	 	 l.add(2);
+	 	 l.add(3);
+	 	 l.add(4);
+	 	 l.remove(2);
+	 	 //System.out.println(l);
+	 	 assertEquals(Arrays.toString(new Object[]{1, 3, 4, null}), l.toString());
+	}
+	
+	@Test(expected=NoSuchElementException.class)
+	public void testRemoveElementException()
+	{
+		 ArrayList l = new ArrayList(4);
+	 	 l.add(1);
+	 	 l.add(2);
+	 	 l.add(3);
+	 	 l.add(4);
+	 	 l.remove(-20);
+	}
+	
+	@Test
+	public void testClear()
+	{
+		 ArrayList l = new ArrayList(4);
+	 	 l.add(1);
+	 	 l.add(2);
+	 	 l.add(3);
+	 	 l.add(4);
+	 	 l.clear();
+	 	 //System.out.println(l);
+	 	 assertEquals(Arrays.toString(new Object[]{null, null, null, null}), l.toString());
+	 	 assertEquals(0, l.size());
+	}
+}
--- src/com/github/rayedchan/datastructures/ArrayList.java
+++ src/com/github/rayedchan/datastructures/ArrayList.java
@@ -1,7 +1,14 @@
 package com.github.rayedchan.datastructures;
 
+import java.util.Arrays;
+import java.util.NoSuchElementException;
+
 /**
  * ArrayList implementation
+ * Invariants:
+ *    size = # of elements in the list
+ *    0 <= size <= capacity
+ *    data array: first SIZE slots filled, followed by nulls 
  * @author rayedchan
  * @see http://www.docjar.com/html/api/java/util/ArrayList.java.html
  * @see http://web.eecs.umich.edu/~aprakash/eecs282/lectures/10-arraylists.pdf
@@ -12,14 +19,9 @@ public class ArrayList
 	// Fields
 	Object[] data; // array of Objects
 	int capacity; // maximum capacity of list
-	int num; // current size of list
+	int size; // current size of list
 	static final int DEFAULT_CAPACITY = 10;
-	
-	// Invariants:
-	// num = # of elements in the list
-	// 0 <= num <= capacity
-	// data array: first num slots filled, followed by nulls 
-	
+		
 	/**
 	 * Constructs an array using the default capacity.
 	 */
@@ -35,8 +37,8 @@ public class ArrayList
 	public ArrayList(int capacity)
 	{
 		this.capacity = capacity; // set field with the provided capacity
-		this.data = new Object[this.capacity]; // null zero  
-		this.num = 0; // zero elements
+		this.data = new Object[this.capacity]; // null elements  
+		this.size = 0; // zero elements
 	}
 	
 	/**
@@ -52,17 +54,64 @@ public class ArrayList
 	/**
 	 * Adds an element in an arbitrary location.
 	 * O(n) - Shifting n - 1 items happens when item is added to the beginning of the list
-	 * @param element	New element to add tp list
-	 * @param index	Index to add the element to
+	 * @param element	New element to add the list
+	 * @param index	Index to add the element to; Must refer to an existing position in the list
+	 * 
+	 * Initial List
+	 * index   0 1 2 3
+	 * content a b c d
+	 * size = 4
+	 *  
+	 * After insertion of x at index 2
+	 * index 	0 1 2 3 4
+	 * content	a b x c d
+	 * size = 5
+	 * 
+	 * 1st iteration
+	 * i = 4
+	 * a[4] = a[3]
+	 * 0 1 2 3 4
+	 * a b c d d
+	 * 
+	 * 2nd iteration
+	 * i = 3
+	 * a[3] = a[2]
+	 * 0 1 2 3 4
+	 * a b c c d
+	 * 
+	 * Replace new content at given index
+	 * a[2] = x
+	 * 0 1 2 3 4
+	 * a b x c d
 	 */
 	public void insertAt(Object element, int index)
 	{
-		// TODO:
+		// Boundary check
+		if(index < 0 || index >= this.size)
+		{
+			throw new IndexOutOfBoundsException("Index out of bound exception");
+		}
+		
+		// Check capacity and current size are the same => Need to expand array
+		if(this.capacity == this.size)
+		{
+			this.expand(); // Create a new array with a larger capacity and copy the content from the old array
+		}
+		
+		// Shift elements rightward by one
+		// Start at the last position (the index with no content)
+		for(int i = this.size; i > index ; i--)
+		{
+			this.data[i] = this.data[i - 1];
+		}
+		
+		this.data[index] = element; // insert element at the given index
+		this.size++; // increment element count in list
 	}
 	
 	/**
-	 * Removes an element from the list.
-	 * @param element	Item to add
+	 * Removes an element from end of the list.
+	 * O(1)
 	 */
 	public void remove()
 	{
@@ -70,19 +119,87 @@ public class ArrayList
 	}
 	
 	/**
-	 * Gets the element at the specified index.
-	 * O(1)
-	 * @param index Array index
+	 * Removes first occurrence of element from the list.
+	 * @param element Element to remove
 	 */
-	public Object getAt(int index)
+	public void remove(Object element)
 	{
-		// Boundary check
-		if(index < 0 || index >= this.num)
+		// Get the index of the given element
+		int index = this.indexOf(element);
+		
+		// Element not found
+		if(index == -1 )
+		{
+			throw new NoSuchElementException("Element not found: " + element);
+		}
+		
+		// Remove element at index
+		this.removeAt(index);
+	}
+	
+	/**
+	 * Removes an element from list at a given index.
+	 * O(n) due to shift operation
+	 * @param index	Index to remove element from
+	 * 
+	 * Initial
+	 * size = 4
+	 * index 	0 1 2 3
+	 * content  a b c d
+	 * 
+	 * After removal of b at index 1
+	 * index	0 1 2 3
+	 * content	a c d null
+	 * 
+	 * 1st iteration of shift
+	 * i = 1
+	 * a[1] = a[2]
+	 * index 	0 1 2 3
+	 * content	a c c d
+	 * 
+	 * 2nd iteration of shift
+	 * i = 2
+	 * a[2] = a[3]
+	 * index	0 1 2 3
+	 * content	a c d d
+	 * 
+	 * Null out last element
+	 * index	0 1 2 3
+	 * content	a c d null
+	 * 
+	 */
+	public void removeAt(int index)
+	{
+		// Check boundary
+		if(index < 0 || index >= this.size)
 		{
 			throw new IndexOutOfBoundsException("Index out of bound exception");
 		}
 		
-		return this.data[index];
+		// shift elements leftward
+		for(int i = index; i < this.size - 1; i++)
+		{
+			this.data[i] = this.data[i + 1];
+		}
+		
+		this.data[this.size - 1] = null; // Null out last element
+		this.size--; // decrement size
+	}
+	
+	/**
+	 * Clears out the entire array with null and set size to zero
+	 * O(n) 
+	 */
+	public void clear()
+	{
+		// Null out array elements
+		for(int i = 0; i < this.size; i++)
+		{
+			this.data[i] = null;
+		}
+		
+		// reset size to zero
+		this.size = 0;
 	}
 	
 	/**
@@ -93,7 +210,7 @@ public class ArrayList
 	public void setAt(Object element, int index)
 	{
 		// Boundary check
-		if(index < 0 || index >= this.num)
+		if(index < 0 || index >= this.size)
 		{
 			throw new IndexOutOfBoundsException("Index out of bound exception");
 		}
@@ -101,13 +218,73 @@ public class ArrayList
 		this.data[index] = element;
 	}
 	
+	
+	/**
+	 * Gets the element at the specified index.
+	 * O(1)
+	 * @param index Array index
+	 */
+	public Object getAt(int index)
+	{
+		// Boundary check
+		if(index < 0 || index >= this.size)
+		{
+			throw new IndexOutOfBoundsException("Index out of bound exception");
+		}
+		
+		return this.data[index];
+	}
+	
+	/**
+	 * Get of index of the specified element
+	 * @param element
+	 * @return Index of given element
+	 */
+	public int indexOf(Object element)
+	{
+		int index = -1; // assume -1 for element not found
+		
+		// iterate array
+		for(int i = 0; i < this.size(); i++)
+		{
+			// found element
+			if(data[i] != null && data[i].equals(element))
+			{
+				return i;
+			}
+		}
+		
+		// element not found
+		return index;
+	}
+	
 	/**
 	 * Fetch number of elements in the list currently
+	 * O(1)
 	 * @return number of elements in list
 	 */
 	public int size()
 	{
-		return this.num;
+		return this.size;
+	}
+	
+	/**
+	 * Determines if the list is empty
+	 * @return true if empty list; false otherwise
+	 */
+	public boolean isEmpty()
+	{
+		return this.size == 0;
+	}
+	
+	/**
+	 * String representation of list
+	 * @return String representation of array
+	 */
+	@Override
+	public String toString()
+	{
+		return Arrays.toString(this.data);
 	}
 	
 	/**
@@ -117,29 +294,29 @@ public class ArrayList
 	private void deleteLastElement()
 	{
 		// No elements to remove
-		if(this.num == 0)
+		if(this.size == 0)
 		{
 			throw new RuntimeException("List is empty: No elements to remove.");
 		}
 		
-		this.num--; // decrement elements in list
-		this.data[num] = null; // remove element
+		this.size--; // decrement elements in list
+		this.data[size] = null; // remove element
 	}
 	
 	/**
 	 * Removes the first element from list.
 	 * O(n) due to shifting elements
 	 */
-	private void deleteFirstElement()
+	public void deleteFirstElement()
 	{
 		// shift elements leftward by one spot
-		for(int index = 0; index < this.num - 1; index++)
+		for(int index = 0; index < this.size - 1; index++)
 		{
 			this.data[index] = this.data[index + 1]; // Replace current with the next element 
 		}
 		
-		this.data[num - 1] = null; // clear the last element
-		num--; // decrement number of elements in list
+		this.data[size - 1] = null; // clear the last element
+		size--; // decrement number of elements in list
 	}
 	
 	/**
@@ -151,13 +328,13 @@ public class ArrayList
 	private void appendElement(Object element)
 	{
 		// Expand array if capacity has been exceeded
-		if(this.num == this.capacity)
+		if(this.size == this.capacity)
 		{
 			this.expand(); 	// Allocate more space to the new array and copy content from old array
 		}
 		
-		this.data[this.num] = element; // add element to end of the list
-		this.num++; // increment number of elements in list
+		this.data[this.size] = element; // add element to end of the list
+		this.size++; // increment number of elements in list
 	}
 	
 	/**
@@ -172,7 +349,7 @@ public class ArrayList
 		Object[] newArray = new Object[newCapacity]; // create new array with a higher capacity
 		
 		// Copy content from the old array to the new array
-		for(int i = 0; i < this.num; i++)
+		for(int i = 0; i < this.size; i++)
 		{
 			newArray[i] = this.data[i];
 		}
@@ -180,5 +357,4 @@ public class ArrayList
 		this.data = newArray; // reference field to the new array
 		this.capacity = newCapacity; // set to the new capacity
 	}
-	
 }
