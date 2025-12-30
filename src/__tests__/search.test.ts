38ee5335318379f946319c65366cda181a6774ea
rayedchan <rayedchan@gmail.com>
2025-12-30 11:57:50 -0500

    add binary search

--- src/__tests__/search.test.ts
+++ src/__tests__/search.test.ts
@@ -1,18 +1,35 @@
-import { linearSearch } from "../search";
+import { binarySearch, linearSearch } from "../search";
 
 describe("linearSearch", () => {
   it("should return true when element is found", () => {
-    const haystack = [1, 3, 5, 7, 9];
+    const haystack: number[] = [1, 3, 5, 7, 9];
     expect(linearSearch(haystack, 5)).toBe(true);
   });
 
   it("should return false when element is not found", () => {
-    const haystack = [1, 3, 5, 7, 9];
+    const haystack: number[] = [1, 3, 5, 7, 9];
     expect(linearSearch(haystack, 4)).toBe(false);
   });
 
   it("should return false for empty array", () => {
     const haystack: number[] = [];
     expect(linearSearch(haystack, 5)).toBe(false);
+  });
+});
+
+describe("binary search", () => {
+  it("should return true when element is found", () => {
+    const sortedArray: number[] = [1, 2, 3, 4, 5];
+    expect(binarySearch(sortedArray, 5)).toBe(true);
+  });
+
+  it("should return false when element is not found", () => {
+    const haystack: number[] = [10, 30, 50, 70, 90];
+    expect(binarySearch(haystack, 5)).toBe(false);
+  });
+
+  it("should return false for empty array", () => {
+    const haystack: number[] = [];
+    expect(binarySearch(haystack, 5)).toBe(false);
   });
 });
--- src/search.ts
+++ src/search.ts
@@ -1,8 +1,30 @@
+// O(n)
 export function linearSearch(haystack: number[], needle: number): boolean {
   for (let i = 0; i < haystack.length; i++) {
     if (haystack[i] === needle) {
       return true;
     }
   }
+  return false;
+}
+
+// O(log n)
+// Array must be sorted
+export function binarySearch(sortedArray: number[], target: number): boolean {
+  let left = 0;
+  let right = sortedArray.length - 1;
+
+  while (left <= right) {
+    let midpoint = Math.floor((left + right) / 2);
+
+    if (sortedArray[midpoint] === target) {
+      return true;
+    } else if (target > sortedArray[midpoint]) {
+      left = midpoint + 1;
+    } else {
+      right = midpoint - 1;
+    }
+  }
+
   return false;
 }
